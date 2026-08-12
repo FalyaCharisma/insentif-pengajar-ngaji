4d544b3d6259ea4bfe54a660b21a792633ff56f7
FalyaCharisma <falyacharisma9@gmail.com>
2026-08-12 07:39:12 +0700

    forum dashboard

--- app/Models/Forum.php
+++ app/Models/Forum.php
@@ -44,8 +44,4 @@ class Forum extends Model
         return 'FRM' . str_pad($number, 5, '0', STR_PAD_LEFT);
     }
 
-    public function forum()
-    {
-        return $this->hasOne(Forum::class);
-    }
 }
